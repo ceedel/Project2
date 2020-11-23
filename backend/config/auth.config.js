ef8ab73f6896a90e0b3e656126a0600d4e4dacd9
ceedel <ciddydella@gmail.com>
2020-11-23 11:50:01 +0200

    nodeJS express backend

---
+++ backend/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "ciddy-secret-key"
+  };
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -9,15 +9,26 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
